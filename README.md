f5d3932f06e0a27d3dc31d4af41859b6fa3804e3
pkslty <pkslty@yandex.ru>
2021-11-05 23:18:59 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # CodingStyle
-Coding style property wrapper
+
+A description of this package.
+
+Property wrapper @CodingStyle returns string decorated in three styles:
+1. Camel case: @CodingSyle(codingStyle: .camelCase) var ...
+2. Snake case: @CodingSyle(codingStyle: .snakeCase) var ...
+3. Kebab case: @CodingSyle(codingStyle: .kebabCase) var ...
+
+Example:
+
+@CodingSyle(codingStyle: .camelCase) var camelCaseString = "String in camel case"
+returns "String In Camel Case"
---
+++ Sources/CodingStyle/CodingStyle.swift
@@ -0,0 +1,66 @@
+import Foundation
+
+@propertyWrapper struct CodingStyle {
+    
+    public var wrappedValue: String {
+        get {
+            get()
+        }
+        set {
+            value = newValue
+        }
+    }
+    enum CodingStyles {
+        case camelCase
+        case snakeCase
+        case kebabCase
+    }
+    
+    private var value: String = ""
+    var codingStyle: CodingStyles
+    
+    init(wrappedValue: String, codingStyle: CodingStyles) {
+        self.value = wrappedValue
+        self.codingStyle = codingStyle
+    }
+    
+    private func get() -> String {
+        
+        var first = value.prefix(1)
+        let last = value.dropFirst()
+        for ch in last {
+            ch.isUppercase ? first.append(contentsOf: " \(ch.lowercased())") :
+            {
+                ch == "-" || ch == "_" ?
+                first.append(" ") :
+                first.append(ch)
+            }()
+        }
+        
+        switch codingStyle {
+        case .camelCase:
+            return value.prefix(1) + value
+                .capitalized
+                .dropFirst()
+                .split(separator: " ")
+                .joined()
+                .split(separator: "-")
+                .joined()
+                .split(separator: "_")
+                .joined()
+        case .snakeCase:
+            return first
+                .split(separator: " ")
+                .joined(separator: "_")
+                .split(separator: "-")
+                .joined(separator: "_")
+       
+        case .kebabCase:
+            return first
+                .split(separator: " ")
+                .joined(separator: "-")
+                .split(separator: "_")
+                .joined(separator: "-")
+        }
+    }
+}
